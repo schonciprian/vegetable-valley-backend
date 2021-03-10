7c27e8a17113a21c078880681046f728fe8c8715
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-10 14:29:48 +0100

    Routes: login and logout routes were added.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,4 +20,5 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::post('/register', [RegisterController::class, 'store']);
-
+Route::post('/login', [LoginController::class, 'store']);
+Route::delete('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
