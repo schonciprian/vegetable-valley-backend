2d68e91d5379143dcb28ed052bf8d7703169dc98
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-03-21 21:02:19 +0100

    User basic information expanded with firstname and lastname.

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -14,7 +14,9 @@ class UserController extends Controller
         $user = Auth::user();
         return response([
             "id" => $user->id,
-            "name" => $user->name,
+            "first_name" => $user->first_name,
+            "last_name" => $user->last_name,
+            "username" => $user->username,
             "email" => $user->email,
             "created_at" => $user->created_at], 201);
     }
@@ -23,10 +25,12 @@ class UserController extends Controller
     {
         User::where('id', $request->id)
             ->update([
-            'name' => $request->name,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'username' => $request->username,
             'email' => $request->email,
         ]);
-        return response(['name' => $request->user()->name], 201);
+        return response(['username' => $request->user()->username], 201);
     }
 
     public function deleteUser(Request $request)
