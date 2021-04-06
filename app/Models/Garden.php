db0dae26489c5a81f9984fc821e2cd5c84afadb4
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-04-06 12:37:11 +0200

    Add: Garden model and migration added and filled with required fields.

---
+++ app/Models/Garden.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Garden extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'cell_id',
+        'cell_name',
+        'cell_picture_url',
+    ];
+}
---
+++ database/migrations/2021_04_06_094626_create_gardens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGardensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gardens', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('cell_id');
+            $table->string('cell_name');
+            $table->string('cell_picture_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gardens');
+    }
+}
