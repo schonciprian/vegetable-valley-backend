1165e31e6f9107bae14b6354159168571b5a3a7b
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-04-22 14:48:59 +0200

    getGardenSize and updateGardenSize function implemented.

--- app/Http/Controllers/Garden/GardenSizeController.php
+++ app/Http/Controllers/Garden/GardenSizeController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers\Garden;
 
+use App\Http\Controllers\Controller;
+use App\Models\GardenSize;
 use Illuminate\Http\Request;
 
 class GardenSizeController extends Controller
 {
-    //
+
+    public function getGardenSize(Request $request)
+    {
+        return Response(GardenSize::select('row_count', 'column_count')
+            ->where('user_id', $request->user()->id)
+            ->get()
+        );
+    }
+
+    public function updateGardenSize(Request $request)
+    {
+        GardenSize::where('user_id', $request->user()->id)
+            ->update([
+                'row_count' => $request->row_count,
+                'column_count' => $request->column_count,
+            ]);
+        return response(['row_count' => $request->row_count,
+                         'column_count' => $request->column_count], 201);
+    }
 }
