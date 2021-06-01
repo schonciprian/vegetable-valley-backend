47dfe66e4672642cef912b512b9ddb4980bb206e
schonciprian <60438162+schonciprian@users.noreply.github.com>
2021-06-01 12:29:17 +0200

    Fix getUserImages to return the tags as well.

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -4,14 +4,51 @@ namespace App\Http\Controllers;
 
 use App\Models\Gallery;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GalleryController extends Controller
 {
     public function getUserImages(Request $request)
     {
-        return Response(Gallery::select('id', 'image_id', 'original_filename', 'format', 'type', 'created_at')
-            ->where('user_id', $request->user()->id)
-            ->orderBy('created_at','DESC')
+//        return Response(Gallery::select(
+//            'galleries.id',
+//            'galleries.image_id',
+//            'galleries.original_filename',
+//            'galleries.format',
+//            'galleries.type',
+//            'galleries.created_at',
+
+//            'image_tags.tag_name',
+//            'image_tags.tag_color'
+//        )
+//            ->leftJoin('image_tags_maps', 'image_tags_maps.image_id', 'galleries.id')
+//            ->leftJoin('image_tags', 'image_tags.id', 'image_tags_maps.tag_id')
+//            ->select('image_tags.tag_name', 'image_tags.tag_color')
+//            ->select(DB::raw("STRING_AGG(image_tags.tag_name, ',')"))
+//            ->where('galleries.user_id', $request->user()->id)
+//            ->groupBy('galleries.id')
+//            ->orderBy('created_at','DESC')
+//            ->get(array(DB::raw("STRING_AGG(image_tags.tag_name, ',')")),'galleries.id')
+//            ->get()
+
+
+        return Response(DB::table('galleries')
+            ->leftJoin('image_tags_maps', 'image_tags_maps.image_id', '=', 'galleries.id')
+            ->leftJoin('image_tags', 'image_tags.id', '=', 'image_tags_maps.tag_id')
+            ->select('galleries.id',
+                'galleries.image_id',
+                'galleries.original_filename',
+                'galleries.format',
+                'galleries.type',
+                'galleries.created_at',
+
+                DB::raw("GROUP_CONCAT(image_tags.tag_name) as tagName"),
+                DB::raw("GROUP_CONCAT(image_tags.tag_color) as tagColor"),
+//                'image_tags.tag_name',
+//                'image_tags.tag_color'
+            )
+            ->where('galleries.user_id', '=', $request->user()->id)
+            ->groupBy('galleries.id')
             ->get()
         );
     }
